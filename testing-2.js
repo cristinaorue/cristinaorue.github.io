8c4f80a53c8ab34285819107e581ea39610dd577
María Cristina Orué <78505592+cristinaorue@users.noreply.github.com>
2023-01-26 14:25:32 -0300

    Update testing-2.js

--- testing-2.js
+++ testing-2.js
@@ -1,5 +1,5 @@
-document.getElementById('title').innerText = 'Something else';
-document.getElementById('text1').innerText = 'Changed with JS';
-document.getElementById('text2').innerText = 'Changed with JS';
-document.getElementById('text3').innerText = 'Changed with JS';
-document.getElementById('text4').innerText = 'Changed with JS';
+document.getElementById('title').innerText = 'Something else - 2';
+document.getElementById('text1').innerText = 'Changed with JS - 2';
+document.getElementById('text2').innerText = 'Changed with JS - 2';
+document.getElementById('text3').innerText = 'Changed with JS - 2';
+document.getElementById('text4').innerText = 'Changed with JS - 2';
