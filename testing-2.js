7d5f387b6de92318f4da2c1f88e13a28c8d01167
María Cristina Orué <78505592+cristinaorue@users.noreply.github.com>
2023-01-26 14:23:53 -0300

    Create testing-2.js

---
+++ testing-2.js
@@ -0,0 +1,5 @@
+document.getElementById('title').innerText = 'Something else';
+document.getElementById('text1').innerText = 'Changed with JS';
+document.getElementById('text2').innerText = 'Changed with JS';
+document.getElementById('text3').innerText = 'Changed with JS';
+document.getElementById('text4').innerText = 'Changed with JS';
