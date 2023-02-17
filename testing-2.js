35660b48687b7e313da3c09e2ed720b6b7ab126e
María Cristina Orué <78505592+cristinaorue@users.noreply.github.com>
2023-02-17 15:35:53 -0300

    Update testing-2.js

--- testing-2.js
+++ testing-2.js
@@ -3,5 +3,3 @@ document.getElementById('text1').innerText = 'Changed with JS - 2';
 document.getElementById('text2').innerText = 'Changed with JS - 2';
 document.getElementById('text3').innerText = 'Changed with JS - 2';
 document.getElementById('text4').innerText = 'Changed with JS - 2';
-
-document.getElementsByClassName("header-title")[0].innerHTML = 'Changed using Swift :)';
